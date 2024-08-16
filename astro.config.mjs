99aee3fba6c21ab2d05f572b3fb4c9f8acb2a2d9
minislel <marcin4kt@gmail.com>
2024-08-16 17:52:02 +0200

    updated deploy yaml

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
+const LIVE_URL = 'https://minislel.github.io'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://minislel.github.io',
