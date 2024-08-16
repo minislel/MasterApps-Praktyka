c4aa34d8f3a29392525df75951c5850bdeadbfc6
minislel <marcin4kt@gmail.com>
2024-08-16 17:27:52 +0200

    finished

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}', "./node_modules/flowbite/**/*.js"],
-	theme: {
-		extend: {},
-	},
+
 	plugins: [
         require('flowbite/plugin')
-    ]
+    ],
+	theme: {
+		colors: {
+		  transparent: 'transparent',
+		  current: 'currentColor',
+		  'almostblack' : '#141414',
+		  'mediumgray' : '#696969',
+		  'almostwhite' : '#fafafa'
+		},
+	  },
 }
